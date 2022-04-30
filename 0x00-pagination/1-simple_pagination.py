79e7463f078fc81522dee9ccd36a21ce21d1c3ad
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-04-30 20:24:05 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
-""" Pagination """
+'''Description: Implement a method named get_page that takes two integer
+                arguments page with default value 1 and page_size with
+                default value 10.
+    - You have to use this CSV file (same as the one presented at the top
+      of the project)
+    - Use assert to verify that both arguments are integers greater than 0.
+    - Use index_range to find the correct indexes to paginate the dataset
+      correctly and return the appropriate page of the dataset (i.e. the
+    correct list of rows).
+    - If the input arguments are out of range for the dataset, an empty list
+      should be returned.
+'''
 
 import csv
 import math
-from typing import List, Tuple
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
@@ -12,6 +25,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        ''' Initialize instance. '''
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -26,8 +40,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
+        ''' Return page of dataset. '''
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """ returns tuple of size two containing start index, end index """
-        pass
+        indices = index_range(page, page_size)
+        start = indices[0]
+        end = indices[1]
+
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
